8cde950b0469b7900e76db6ee15db99fb1eadf72
peter <petr-niz1@yandex.ru>
2020-11-08 04:59:23 +0500

    cleanup

--- docker-build-arm64.cmd
+++ docker-build-arm64.cmd
@@ -2,6 +2,6 @@ dotnet restore
 dotnet build -c release /nowarn:CS1591
 dotnet publish -c release -o ./docker/build
 rmdir ./docker/build /S /Q
-docker build ./docker -t pets-webapi --build-arg ARCH=arm64
+docker build ./docker -f ./docker/Dockerfile-arm64v8 -t pets-webapi --build-arg ARCH=arm64
 docker tag pets-webapi maiznpetr/pets-webapi:v1-arm64v8
 docker push maiznpetr/pets-webapi:v1-arm64v8
