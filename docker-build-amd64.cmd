83258da2114093d570bb2b8d22bc1507ddf12a09
peter <petr-niz1@yandex.ru>
2021-01-09 03:51:15 +0500

    bump

--- docker-build-amd64.cmd
+++ docker-build-amd64.cmd
@@ -3,5 +3,5 @@ dotnet build -c release /nowarn:CS1591
 dotnet publish -c release -o ./docker/build
 rmdir ./docker/build /S /Q
 docker build ./docker -f ./docker/Dockerfile -t pets-webapi --build-arg ARCH=amd64
-docker tag pets-webapi maiznpetr/pets-webapi:v1.1-amd64
-docker push maiznpetr/pets-webapi:v1.1-amd64
+docker tag pets-webapi maiznpetr/pets-webapi:v1.2-amd64
+docker push maiznpetr/pets-webapi:v1.2-amd64
--- docker-build-manifest.cmd
+++ docker-build-manifest.cmd
@@ -1,2 +1,2 @@
-docker manifest create maiznpetr/pets-webapi:v1.1 --amend maiznpetr/pets-webapi:v1.1-amd64 --amend maiznpetr/pets-webapi:v1.1-arm64v8
-docker manifest push maiznpetr/pets-webapi:v1.1
+docker manifest create maiznpetr/pets-webapi:v1.2 --amend maiznpetr/pets-webapi:v1.2-amd64 --amend maiznpetr/pets-webapi:v1.2-arm64v8
+docker manifest push maiznpetr/pets-webapi:v1.2
