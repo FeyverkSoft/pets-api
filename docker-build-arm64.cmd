29b4c7dc8fc850279ef2f9bd9b7b313a373607de
peter <petr-niz1@yandex.ru>
2021-02-06 20:14:27 +0500

    пока что мультитагрет, до тех пор пока не перееду на net5

--- docker-build-arm64.cmd
+++ docker-build-arm64.cmd
@@ -1,6 +1,6 @@
 dotnet restore
 dotnet build -c release /nowarn:CS1591
-dotnet publish -c release -o ./docker/build
+dotnet publish --framework netcoreapp3.1 -c release -o ./docker/build
 rmdir ./docker/build /S /Q
 docker build ./docker -f ./docker/Dockerfile-arm64v8 -t pets-webapi --build-arg ARCH=arm64
  docker tag pets-webapi maiznpetr/pets-webapi:v1.2.7-arm64v8
