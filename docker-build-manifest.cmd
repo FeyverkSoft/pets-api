a1a01b7708eb1b9cc53933dd5c8d9f88d6ff861b
peter <petr-niz1@yandex.ru>
2021-03-14 02:12:52 +0500

    bump version

--- docker-build-manifest.cmd
+++ docker-build-manifest.cmd
@@ -1,2 +1,2 @@
-docker manifest create maiznpetr/pets-webapi:v1.2.16 --amend maiznpetr/pets-webapi:v1.2.16-amd64 --amend maiznpetr/pets-webapi:v1.2.16-arm64v8
-docker manifest push maiznpetr/pets-webapi:v1.2.16
+docker manifest create maiznpetr/pets-webapi:v1.2.17 --amend maiznpetr/pets-webapi:v1.2.17-amd64 --amend maiznpetr/pets-webapi:v1.2.17-arm64v8
+docker manifest push maiznpetr/pets-webapi:v1.2.17
