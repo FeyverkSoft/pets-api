f4463cd469c65da429ebe72fd66d847cbc5917e5
peter <petr-niz1@yandex.ru>
2021-04-12 02:06:53 +0500

    bump version and move project

--- docker-build-manifest.cmd
+++ docker-build-manifest.cmd
@@ -1,2 +1,2 @@
-docker manifest create maiznpetr/pets-webapi:v1.2.23 --amend maiznpetr/pets-webapi:v1.2.23-amd64 --amend maiznpetr/pets-webapi:v1.2.23-arm64v8
-docker manifest push maiznpetr/pets-webapi:v1.2.23
+docker manifest create maiznpetr/pets-webapi:v1.2.24 --amend maiznpetr/pets-webapi:v1.2.24-amd64 --amend maiznpetr/pets-webapi:v1.2.24-arm64v8
+docker manifest push maiznpetr/pets-webapi:v1.2.24
