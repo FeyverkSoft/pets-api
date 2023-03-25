03a289fe517004ce3298b9fbc007516e75abd656
peter <petr-niz1@yandex.ru>
2023-03-25 19:12:30 +0500

    net6

---
+++ src/Pets.Infrastructure/Mediatr/DiExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Pets.Infrastructure.Mediatr;
+
+using MediatR;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+public static class DiExtensions
+{
+    public static IServiceCollection AddMediatorCommandDedublicateBehaviour(this IServiceCollection services)
+    {
+        services.TryAddSingleton<IMediatorThrottlingService, MediatorThrottlingService>();
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CommandDedublicateBehaviour<,>));
+        return services;
+    }
+}
---
+++ src/Pets.Infrastructure/Mediatr/MediatRDedublicateExecutionAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace Pets.Infrastructure.Mediatr;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public sealed class MediatRDedublicateExecutionAttribute : Attribute
+{
+    /// <summary>
+    ///     Время задержки от повторного вызова
+    /// </summary>
+    public Int32 ThrottlingTimeMs { get; init; } = 1000;
+
+    /// <summary>
+    ///     Название свойства ключа
+    /// </summary>
+    public String KeyPropertyName { get; init; }
+
+    /// <summary>
+    ///     Название ключей
+    ///     если задан KeyPropertyName, то это свойство игнорируется
+    /// </summary>
+    public String[]? KeyPropertyNames { get; init; }
+}
