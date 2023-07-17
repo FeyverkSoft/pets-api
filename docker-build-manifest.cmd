3b4a18714636eeed420ba08e7786772e8b5afb51
peter <petr-niz1@yandex.ru>
2023-07-17 15:49:48 +0500

    .

--- docker-build-manifest.cmd
+++ docker-build-manifest.cmd
@@ -1,2 +1,2 @@
-docker manifest create maiznpetr/pets-webapi:v1.2.36 --amend maiznpetr/pets-webapi:v1.2.36-amd64 --amend maiznpetr/pets-webapi:v1.2.36-arm64v8
-docker manifest push maiznpetr/pets-webapi:v1.2.36
+docker manifest create maiznpetr/pets-webapi:v1.2.37 --amend maiznpetr/pets-webapi:v1.2.37-amd64 --amend maiznpetr/pets-webapi:v1.2.37-arm64v8
+docker manifest push maiznpetr/pets-webapi:v1.2.37
