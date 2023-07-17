ea49e37301b7af1d1bdff6caef4b04c77e7589d8
peter <petr-niz1@yandex.ru>
2023-07-17 15:49:34 +0500

    Bump Rabbita

--- src/Pets.Domain/Pet/Events/PetChangeDescription.cs
+++ src/Pets.Domain/Pet/Events/PetChangeDescription.cs
@@ -8,4 +8,4 @@ using Rabbita.Core.Event;
 /// <param name="PetId">Идентификатор питомца</param>
 /// <param name="Date">Дата события</param>
 /// <param name="Body">Новое тело описания</param>
-public sealed record PetChangeDescription(Guid PetId, String Date, DateTime Body) : IEvent;
+public sealed record PetChangeDescription(Guid PetId, String Body, DateTime Date) : IEvent;
